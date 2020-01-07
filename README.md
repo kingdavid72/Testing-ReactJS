ab943cc630b942f63418f6a4294b00144d28a902
Jae Mun Choi <jaemunc@gmail.com>
2020-01-07 11:37:16 -0600

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Testing-ReactJS
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Wellcome to Calici</h1>
+      <h2>어서들 오세유 ^^ </h2>
+    </div>
+  );
+}
